1ba3bb4012ff808b44a2ad1fcca18ed7b4970f6a
omolewa <akinyomiomolewa30gmail.com>
2017-06-06 14:15:58 +0100

    made changes

--- lab1/arithGeo.js
+++ lab1/arithGeo.js
@@ -1,13 +1,14 @@
 module.exports = {
-function aritGeo(arr) { 
+function ArithGeo(arr) { 
 var Arithmetic = "Arithmetic";
 var Geometric = "Geometric ";
-for(var i = 0; i < arr.length; i++)
-  {
-    if (arr === []){
+if (arr.length === 0){
       return 0;
     }
-    else if(arr[i + 1]-arr[i]===arr[arr.length - 1]-arr[arr.length - 2]){
+for(var i = 0; i < arr.length; i++)
+  {
+    
+    if(arr[i + 1]-arr[i]===arr[arr.length - 1]-arr[arr.length - 2]){
       return Arithmetic;
     }
     else if(arr[i+1]/arr[i] ===arr[i+2]/arr[i+1]){
@@ -18,5 +19,4 @@ for(var i = 0; i < arr.length; i++)
        return -1;
     }
   }
-}
 }
